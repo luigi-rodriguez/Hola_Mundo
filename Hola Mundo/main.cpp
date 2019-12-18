3f8087eca29919ace7a05e2dcc7af6986a47bf22
luigi-rodriguez <57381035+luigi-rodriguez@users.noreply.github.com>
2019-12-18 15:28:21 -0600

    commit 1
    Hola Mundo Simple en C++

---
+++ Hola Mundo/main.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h> //libreria por defecto y para identificar c++
+#include <stdlib.h> //libreria para mantener el programa
+
+int main() // inicio de programa
+{                       // \n para teclear el programa
+    printf(" \t \t \t \t HOLA MUNDO, Como Estais !!! \n \n"); // (printf) funcion de la libreria stdio
+    system("PAUSE"); //permite pausar el programa
+    return 0; //no regresa nada
+}
